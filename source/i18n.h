2dbefd08dd84fc3767ec7fcc4f8ad533eeb60161
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2012-05-27 16:36:59 +0200

    Created doc/ folder for documentation and added document to specify the version numbering guidelines

--- source/i18n.h
+++ source/i18n.h
@@ -7,7 +7,7 @@
 /*
  * Constant for the version number of the application
  */
-const std::string strVersionNumber = "0.3.4.3";
+const std::string strVersionNumber = "0.3.4.4.75";
 
 /* This variable sets the language used in the menus
  * using constants defined in globals.h
