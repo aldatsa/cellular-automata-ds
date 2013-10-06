37172eb9db953b0a7f0935d2ef16f55843388283
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2013-10-07 00:20:27 +0200

    Update the version number

--- source/i18n.h
+++ source/i18n.h
@@ -7,7 +7,7 @@
 /*
  * Constant for the version number of the application
  */
-const std::string strVersionNumber = "0.10.1.1.146";
+const std::string strVersionNumber = "0.10.1.4.149";
 
 /*
  * The strings of text used in the app in the default language (english).
