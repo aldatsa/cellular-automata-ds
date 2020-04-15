0041b74f952c429bfd29f3c22b4002432c1fd8da
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2020-04-15 21:26:43 +0200

    Update year

--- source/menus.cpp
+++ source/menus.cpp
@@ -94,7 +94,7 @@ int printCredits()
     printf("Cellular Automata DS\n");
     printf("%s\n", stringVersion.c_str());
     printf("\n");
-    printf("2012-2017 GNU GPLv3\n");
+    printf("2012-2020 GNU GPLv3\n");
     printf("Asier Iturralde Sarasola\n");
     printf("\n");
 
